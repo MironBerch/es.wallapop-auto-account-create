6aae4378767bb63083ea496624a7d05083e5a580
Miron Berchetov <106734953+MironBerch@users.noreply.github.com>
2024-02-20 00:10:33 +0300

    Update 3.py

--- src/3.py
+++ src/3.py
@@ -1,21 +1,47 @@
-import time
-from seleniumwire import undetected_chromedriver
 
+import requests
+import sys
 
-def browser_with_proxy():
-    """Браузер с прокси без авторизации в формате ip:port"""
-    chrome_options=undetected_chromedriver.ChromeOptions()
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--ignore-ssl-errors')
+a = 'http://local.adspower.net:50325/api/v1/browser/start?user_id=jec5532'
+host = '192.168.42.70'
+port = '30001'
+"""
+group = 'testgroup'
+create_group = requests.post(
+    'http://local.adspower.net:50325/api/v1/group/create',
+    json={'group_name': group}
+).json()
+print(create_group)
+"""
+"""
+create = requests.post(
+    'http://local.adspower.net:50325/api/v1/user/create', 
+    json={
+        'group_id': 3662521,
+        'user_proxy_config': {
+            'proxy_soft': '922S5',
+            'proxy_type': 'socks5',
+            'proxy_host': host,
+            'proxy_port': port,
+        },
+        'country': 'it',
+    },
+).json()
+if create:
+    print(create)
+    sys.exit()
+"""
+create = requests.get(
+    'http://local.adspower.net:50325/api/v1/user/list', 
+).json()
+print(create)
 
-    wire_options = {
-        'proxy': {
-            'https': 'socks5://95.245.161.134:30000/',
-        }
-    }
-    driver = undetected_chromedriver.Chrome(seleniumwire_options=wire_options, options=chrome_options)
-    #driver.uc()
-    driver.get("https://2ip.ru")
-    time.sleep(150)
-
-browser_with_proxy()
+#{"ua_browser":["chrome"],"ua_version":["80"],"ua_system_version":["Windows 10"]}
+{'data': 
+ {'list': 
+  [
+      {'name': '', 'domain_name': '', 'created_time': '1708376366', 'ip': '87.6.29.120', 
+       'ip_country': 'it', 
+       'password': '', 
+       'fbcc_proxy_acc_id': '0', 'ipchecker': 'ip2location'
+       #получить  бинд ип и кайф
