d197e0f8d30df2573485707a94cf21b0a30061f7
Miron Berchetov <106734953+MironBerch@users.noreply.github.com>
2024-02-22 20:57:34 +0300

    Update 3.py

--- src/3.py
+++ src/3.py
@@ -211,3 +211,30 @@ time.sleep(5)
 driver.quit()
 requests.get(close_url)
                   
+#############################################################################################################################
+
+import requests,time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import random 
+import string
+from selenium.webdriver.chrome.service import Service
+import sys
+
+ads_id = "jeejwpf"
+open_url = "http://local.adspower.net:50325/api/v1/browser/start?user_id=" + ads_id
+close_url = "http://local.adspower.net:50325/api/v1/browser/stop?user_id=" + ads_id
+resp = requests.get(open_url).json()
+if resp["code"] != 0:
+    sys.exit()
+print(resp)
+chrome_driver = resp["data"]["webdriver"]
+service = Service(chrome_driver)
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", resp["data"]["ws"]["selenium"])
+driver = webdriver.Chrome(service=service, options=chrome_options)
+print(driver.title)
+driver.get("https://www.baidu.com")
+time.sleep(5)
+driver.quit()
+requests.get(close_url)
